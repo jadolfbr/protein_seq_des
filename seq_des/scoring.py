0d57276d9215049d57d1a9ab38bfb627eff11372
Jared Adolf-Bryfogle <jadolfbr@gmail.com>
2022-08-01 15:54:28 -0400

    change get_log_p to use pose or path.

--- seq_des/scoring.py
+++ seq_des/scoring.py
@@ -8,10 +8,14 @@ import seq_des.common.atoms as atoms
 
 
 
-def get_log_p(pose, logdir, classifiers=None):
+def get_log_p(pose_or_pdbpath, logdir, classifiers=None):
     """
     Returns the logp_mean and logp per residue from seq des inference code.
-    Pass models in to speed up inference speed
+    Pass models in to speed up inference speed.
+
+    Accepts either a rosetta pose or a pdb path.
+    If Rosetta pose, it dumps the pose as logdir/pose before reading it back in.
+
     :param logdir:
     :return:
     """
@@ -23,10 +27,16 @@ def get_log_p(pose, logdir, classifiers=None):
         for classifier in classifiers:
             classifier.eval()
 
-    (res_label, log_p_per_res, log_p_mean, logits, chi_feat, chi_angles,
-     chi_mask,) = sampler_util.get_energy(
-        classifiers, pose, log_path=logdir, include_rotamer_probs=1, use_cuda=torch.cuda.is_available(),
-    )
+    if (type(pose_or_pdbpath) == str):
+        (res_label, log_p_per_res, log_p_mean, logits, chi_feat, chi_angles,
+         chi_mask,) = sampler_util.get_energy(
+            classifiers, pdb=pose_or_pdbpath, log_path=logdir, include_rotamer_probs=1, use_cuda=torch.cuda.is_available(),
+        )
+    else:
+        (res_label, log_p_per_res, log_p_mean, logits, chi_feat, chi_angles,
+         chi_mask,) = sampler_util.get_energy(
+            classifiers, pose=pose_or_pdbpath, log_path=logdir, include_rotamer_probs=1, use_cuda=torch.cuda.is_available(),
+        )
 
     p_mean = log_p_mean.item()
 
